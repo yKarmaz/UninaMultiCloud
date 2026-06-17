8bd414e20989354803b3b4f2c0648a75c9c2e240
mario <mario@Karmaz.home-life.hub>
2026-06-17 13:05:59 +0200

    creazione Utente

--- src/entities/Utente.java
+++ src/entities/Utente.java
@@ -1,5 +1,60 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Utente {
 
+	private String Matricola;
+	private String nome;
+	private String cognome;
+	private String username;
+	private String email;
+	private String password;
+	private ArrayList <ElementoMultimediale> elementiPubblicati;
+	private ArrayList <Playlist> miePlaylist;
+	
+	public Utente(String matricola, String nome, String cognome, String username, String email, String password) {
+		Matricola = matricola;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getMatricola() {
+		return Matricola;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public boolean modificaPassword(String oldPassword, String newPassword) {
+		
+	}
+	
+	public ArrayList <ElementoMultimediale> getElementiPubblicati(){
+		
+	}
+	
+	public ArrayList <PlayList> getMiePlaylist(){
+		
+	}
 }
